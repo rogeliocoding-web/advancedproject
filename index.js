dc8e31b0f3d948d1d66694dcac67598027deaa4f
rogeliocoding-web <rogeliocoding@gmail.com>
2026-03-20 22:48:31 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+function toggleContrast() {
+    document.body.classList.toggle(" dark-theme");
+};
 
 
 function contact(event) {
@@ -25,7 +40,7 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) { 
         isModalOpen = false
