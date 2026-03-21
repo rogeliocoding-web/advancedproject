70011c7c529a14a3ffd5b8e6f15e9d6fe8428ec3
rogeliocoding-web <rogeliocoding@gmail.com>
2026-03-20 21:08:48 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-// template_bd3y7ry
-// service_0ak3vcu
-// aFORv6KVYrWVfRyD3
 
-function contact (event) {
-    event.preventDefault();
-    
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_0ak3vcu",
+      "template_bd3y7ry",
+      event.target,
+      "aFORv6KVYrWVfRyD3",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on rogeliocoding@gmail.com",
+      );
+    });
 }
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) { 
+        isModalOpen = false
+        return document.body.classList.remove("modal--open"); 
+        
+    }
+    isModalOpen = true;
+   document.body.classList += " modal--open";
+}
+
