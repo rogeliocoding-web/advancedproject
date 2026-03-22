ec2a48ffaceca0c3d500218b4550ec158ef0007a
rogeliocoding-web <rogeliocoding@gmail.com>
2026-03-21 18:55:18 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let isModalOpen = false;
 let contrastToggle = false;
 
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX;
+    const y = event.clientY;
+
+    for (let i = 0; i < shapes.length; ++i) { 
+        shapes{i}.style.transform = `translate(10%, 10%)`;
+
+}
+}
+
+
+
+
 function toggleContrast() {
     contrastToggle = !contrastToggle;
     if (contrastToggle) {
