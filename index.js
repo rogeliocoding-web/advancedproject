b7ad88a7b85225c058f2a801bf492293d1b6f37b
rogeliocoding-web <rogeliocoding@gmail.com>
2026-03-22 13:38:00 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
     const shapes = document.querySelectorAll(".shape");
-    const x = event.clientX;
-    const y = event.clientY;
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
 
     for (let i = 0; i < shapes.length; ++i) { 
-        shapes[i].style.transform = `translate(${x * speed}px, ${y * speed}px)`;
+        const isOdd = i % 2 !== 0; 
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
 
 }
 }
+
 
 
 
